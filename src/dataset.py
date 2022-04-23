dab1cbc948323dcad2af2cf51a30f879cb63f7f4
Mohamed Ghazal <mohamedghazal@MBP-von-Mohamed.fritz.box>
2022-04-23 22:42:18 +0200

    Downloading the dataset and classifying the motion data now working

--- src/dataset.py
+++ src/dataset.py
@@ -1,122 +1,93 @@
-import torch
-import torchvision.transforms as transforms
 import os
+import shutil
+import requests
+import tempfile
 import errno
 import zipfile
+from tqdm.auto import tqdm
 
 # Change this to the path where you want to download the dataset to
-DEFAULT_ROOT = '/data/dataset'
+DEFAULT_ROOT = 'Developer/bachelorarbeit/data'
+URL = 'https://motion-annotation.humanoids.kit.edu/downloads/4/'
+# BUFFER_SIZE = 32 * 2048 * 2048
 
 
-class MotionDataset(torch.utils.data.Dataset):
+class Motion:
+
+    def __init__(self, meta, format_, annotation, motion_data):
+        self.meta = meta
+        self.annotation = annotation
+        self.format_ = format_
+        self.motion_data = motion_data
+
+    def parse(self):
+        pass
+
+
+class MotionDataset:
     urls = [
         'https://motion-annotation.humanoids.kit.edu/downloads/4/',
     ]
 
     def __init__(self, root=DEFAULT_ROOT, train=True, transform=None):
         self.root = os.path.expanduser(root)
-        self.train = train  # training set or test set
-        self.transform = transform
-        self.download()
-
-        if self.train:
-            self.train_data = torch.load(os.path.join(self.root, 'training_data.pt'))
-            self.train_labels = torch.load(os.path.join(self.root, 'training_labels.pt'))
-        else:
-            self.test_data = torch.load(os.path.join(self.root, 'test_data.pt'))
-            try:
-                self.test_labels = torch.load(os.path.join(self.root, 'test_labels.pt'))
-            except FileNotFoundError as e:
-                print(f'No test labels found at {e.filename}!')
-
-                class Dummy:
-                    def __getitem__(self, item):
-                        return None
-                self.test_labels = Dummy()
-
-    def __getitem__(self, index):
-        """
-        Args:
-            index (int): Index
-
-        Returns:
-            tuple: (image, target) where target is index of the target class.
-        """
-        if self.train:
-            img, target = self.train_data[index], self.train_labels[index]
-        else:
-            img, target = self.test_data[index], self.test_labels[index]
-
-        if self.transform is not None:
-            img = self.transform(img)
-
-        return img, target
-
-    def __len__(self):
-        if self.train:
-            return len(self.train_data)
-        else:
-            return len(self.test_data)
-
-    def _check_exists(self):
-        return os.path.exists(os.path.join(self.root, 'training_data.pt')) and \
-            os.path.exists(os.path.join(self.root, 'test_data.pt')) and \
-            os.path.exists(os.path.join(self.root, 'training_labels.pt'))
+        print(self.root)
 
     def download(self):
-        from six.moves import urllib
+        root = os.path.expanduser(DEFAULT_ROOT)
+        print('Downloading the dataset...')
+        with requests.get(URL, stream=True) as request:
+            print(request.headers)
+            dataset_length = int(request.headers.get('Content-Length'))
+            with tqdm.wrapattr(
+                request.raw,
+                'read',
+                total=dataset_length,
+                desc='',
+            ) as raw_data:
 
-        if self._check_exists():
-            return
+                with open(
+                    f'{os.path.basename(request.url)}',
+                    'wb'
+                ) as dataset:
+                    shutil.copyfileobj(raw_data, dataset)
+        print('Extracting the dataset...')
+        with zipfile.ZipFile(dataset, 'r') as zip_file:
+            zip_file.extract(
+                os.path.join(
+                    os.expanduser(DEFAULT_ROOT),
+                    '2017..',
+                ),
+                path=root,
+            )
+        print('Done')
 
-        # download files
-        try:
-            os.makedirs(self.root)
-        except OSError as e:
-            if e.errno == errno.EEXIST:
-                pass
-            else:
-                raise
-
-        for url in self.urls:
-            print('Downloading ' + url)
-            data = urllib.request.urlopen(url)
-            filename = 'strange_symbols.tar.gz'
-            file_path = os.path.join(self.root, filename)
-            with open(file_path, 'wb') as f:
-                f.write(data.read())
-            zipfile.open(file_path, mode='r:gz').extractall(self.root)
-            os.unlink(file_path)
-
-    def __repr__(self):
-        fmt_str = 'Dataset ' + self.__class__.__name__ + '\n'
-        fmt_str += '    Number of datapoints: {}\n'.format(self.__len__())
-        tmp = 'train' if self.train is True else 'test'
-        fmt_str += '    Split: {}\n'.format(tmp)
-        fmt_str += '    Root Location: {}\n'.format(self.root)
-        return fmt_str
-
-
-def to_float(x):
-    x = x.float()
-    x /= 255
-    x *= 2
-    x -= 1
-    return x.unsqueeze(-3)
-
-
-DEFAULT_TRANSFORMS = transforms.Lambda(to_float)
-
-
-def get_strange_symbols_train_loader(batch_size, transform=DEFAULT_TRANSFORMS):
-    trainset = MotionDataset(root=DEFAULT_ROOT, train=True,  transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
-    return trainloader
+    def parse(self):
+        print('Parsing the dataset...')
+        current_directory = os.getcwd()
+        format_ = input(
+            'Enter the type of format type you want to use. '
+            'The possible formats are MMM, C3D: '
+        )
+        os.chdir('data/motion_dataset')
+        types, formats = [], []
+        for file in os.listdir():
+            name, file_format = file.split('.')
+            id_, type_ = name.split('_')
+            types.append(type_)
+            formats.append(file_format)
+            if format_ == file_format.lower():
+                with open(file, 'r') as file:
+                    pass
+                print(f'id is {id_}, type is {type_}')
+        print(
+            f'The types are {set(types)}',
+            f'and the formats are {set(formats)}',
+        )
+        os.chdir(current_directory)
 
 
-def get_strange_symbols_train_data(root=DEFAULT_ROOT, transform=DEFAULT_TRANSFORMS):
-    return MotionDataset(root=root, train=True, transform=transform)[:]
+if __name__ == '__main__':
+    motion_dataset = MotionDataset()
+    motion_dataset.parse()
 
-
-def get_strange_symbols_test_data(root=DEFAULT_ROOT, transform=DEFAULT_TRANSFORMS):
-    return MotionDataset(root=root, train=False, transform=transform)[:]
---
+++ src/utils.py
@@ -0,0 +1,26 @@
+import os
+import shutils
+from dataset import DEFAULT_ROOT
+from functools import partial
+
+
+def check_exists():
+    return os.path.isdir(
+        os.path.join(
+            os.path.expanduser(''),
+            'motion_dataset')
+    )
+
+
+def pseudo_wrapper(function, arguments=arguments):
+    def func(*args, **kwargs):
+        root = os.path.expanduser(DEFAULT_ROOT)
+        check_exists()
+        os.chdir(os.path.join(os.path.expanduser(''), DEFAULT_ROOT))
+        function(*args, **kwargs)
+        os.chdir(root)
+        return function(*args, **kwargs)
+    return func
+
+
+env_wrapper = partial(pseudo_wrapper, argument=arguments)
